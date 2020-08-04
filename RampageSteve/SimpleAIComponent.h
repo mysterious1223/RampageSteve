dbb0d59108cef82ebdf7ae7a1eed9720522082f8
Kevin Singh <kevinsingh.developer@gmail.com>
2020-08-03 20:58:57 -0400

    Header organization 20200803

--- RampageSteve/SimpleAIComponent.h
+++ RampageSteve/SimpleAIComponent.h
@@ -11,6 +11,13 @@
 #include "Globals.h"
 
 
+// We dont need this shit until the next project where we will have different AIs
+//enum class AIType;
+// What kind of AI?
+//enum class AIType  {
+  
+//};
+
 
 class Component;
 class SimpleAIComponent : public Component
@@ -26,10 +33,43 @@ public:
 	void updateInput(const float& dt, sf::Event*);
 
 	void updateRender(sf::RenderTarget* target);
-
+    
+    // Control functions
+    
+    // Move to current target
+    [[nodiscard]]
+    bool moveToTarget ();
+    
+    
+    // move to specific position
+    [[nodiscard]]
+    bool moveToPos (const Entity&);
+    
+    [[nodiscard]]
+    bool moveToPos (const sf::Vector2f&);
+    
+    
+    
+    // set target IF one is not currently set
+    [[nodiscard]]
+    bool setTarget (const Entity&);
+    
+    
+    // set sphere of influence
+    inline void setSphereOfInfluence (const float& radius) {this->_detectionRadius = radius;};
+    
+    
+    
 	~SimpleAIComponent();
 private:
 
+    
+    // Trigger actions when something enters withing the circle around this entity
+    float _detectionRadius;
+    
+    
+    // Maybe swap for a unique pointer?
+    Entity &_target;
 };
 
 
